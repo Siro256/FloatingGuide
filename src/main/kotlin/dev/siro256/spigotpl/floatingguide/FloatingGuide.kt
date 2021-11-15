e3ec94299142ef236ff3e790123a76d2dc0a8fd6
Siro_256 <siro@siro256.dev>
2021-11-15 14:16:42 +0900

    Register wand listener to the server

--- src/main/kotlin/dev/siro256/spigotpl/floatingguide/FloatingGuide.kt
+++ src/main/kotlin/dev/siro256/spigotpl/floatingguide/FloatingGuide.kt
@@ -2,6 +2,7 @@ package dev.siro256.spigotpl.floatingguide
 
 import dev.siro256.spigotpl.floatingguide.command.TestCommand
 import dev.siro256.spigotpl.floatingguide.command.WandCommand
+import dev.siro256.spigotpl.floatingguide.listener.WandListener
 import org.bukkit.Location
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
@@ -14,6 +15,11 @@ class FloatingGuide: JavaPlugin() {
     }
 
     override fun onEnable() {
+        //Register event listeners
+        listOf(
+            WandListener
+        ).forEach { server.pluginManager.registerEvents(it, this) }
+
         //Register commands
         mapOf(
             "test" to TestCommand,
